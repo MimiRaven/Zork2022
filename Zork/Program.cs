9c1fa22c40791ba9a4f74a22ebd61a36cd5d64d3
MimiRaven <89601792+MimiRaven@users.noreply.github.com>
2022-08-31 15:05:50 -0400

    Zork 1.3

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -8,53 +8,45 @@ namespace Zork
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine();
-            Commands command = ToCommand(inputString.Trim().ToUpper());
-            //Console.WriteLine(command);
+            bool isRunning = true;
 
-            if (command == Commands.QUIT)
+            while (isRunning)
             {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if (command == Commands.LOOK)
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-            else if (command == Commands.NORTH)
-            {
-                Console.WriteLine("You moved north.");
-            }
-            else if (command == Commands.SOUTH)
-            {
-                Console.WriteLine("You moved south.");
-            }
-            else if (command == Commands.EAST)
-            {
-                Console.WriteLine("You moved east.");
-            }
-            else if (command == Commands.WEST)
-            {
-                Console.WriteLine("You moved west.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
+                Console.Write("> ");
+                string inputString = Console.ReadLine().Trim();
+                Commands command = ToCommand(inputString);
+
+                string outputString;
+
+                switch (command)
+                {
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                    case Commands.NORTH:
+                        outputString = $"You moved {command}.";
+                        break;
+
+                    case Commands.LOOK:
+                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        break;
+
+                    case Commands.QUIT:
+                        isRunning = false;
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    default:
+                        outputString = $"Unknown command: {inputString}";
+                        break;
+                }
+
+                Console.WriteLine(outputString);
             }
         }
 
         private static Commands ToCommand(string commandString)
         {
-          //Commands command;
-
-        //   if (Enum.TryParse<Commands>(commandString, true, out Commands result))
-        //   {
-        //       return result;
-        //   }
-        //   else
-        //   {
-        //       return Commands.UNKNOWN;
-        //   }
-
             try
             {
                 return (Commands)Enum.Parse(typeof(Commands), commandString, true);
@@ -63,61 +55,6 @@ namespace Zork
             {
                 return Commands.UNKNOWN;
             }
-
-          //return Enum.Parse<Commands>(commandString, true);
-
-       //   switch (commandString)
-       //   {
-       //       case "QUIT":
-       //           command = Commands.QUIT;
-       //           break;
-       //       case "NORTH":
-       //           command = Commands.NORTH;
-       //           break;
-       //       case "SOUTH":
-       //           command = Commands.SOUTH;
-       //           break;
-       //       case "EAST":
-       //           command = Commands.EAST;
-       //           break;
-       //       case "WEST":
-       //           command = Commands.WEST;
-       //           break;
-       //       default:
-       //           command = Commands.UNKNOWN;
-       //           break;
-       //   }
-
-          //return command;
-
-        // if (commandString == "QUIT")
-        // {
-        //     command = Commands.QUIT;
-        // }
-        // else if (commandString == "LOOK")
-        // {
-        //     command = Commands.LOOK;
-        // }
-        // else if (commandString == "NORTH")
-        // {
-        //     command = Commands.NORTH;
-        // }
-        // else if (commandString == "SOUTH")
-        // {
-        //     command = Commands.SOUTH;
-        // }
-        // else if (commandString == "EAST")
-        // {
-        //     command = Commands.EAST;
-        // }
-        // else if (commandString == "WEST")
-        // {
-        //     command = Commands.WEST;
-        // }
-        // else
-        // {
-        //     command = Commands.UNKNOWN;
-        // }
         }
     }
 }
