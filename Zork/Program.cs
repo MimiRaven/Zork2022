2c1803c7eb715d9c2ae60e3c76b3130cec5ffa5e
MimiRaven <89601792+MimiRaven@users.noreply.github.com>
2022-09-26 14:58:52 -0400

    Zork 4.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Security.Cryptography.X509Certificates;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Zork
 {
@@ -31,21 +32,15 @@ namespace Zork
                     break;
 
                 case Commands.EAST when currentColumn < rooms.GetLength(1) - 1:
-
                     currentColumn++;
                     didMove = true;
                     break;
 
                 case Commands.WEST when currentColumn > 0:
-
                     currentColumn--;
                     didMove = true;
                     break;
             }
-
-
-
-
             return didMove;
         }
 
@@ -58,7 +53,9 @@ namespace Zork
 
         static void Main(string[] args)
         {
-            InitializeRoomDescription();
+            const string defaultRoomsFilename = "Rooms.txt";
+            string roomsfilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
+            InitializeRoomDescription(@"Content\Rooms.txt");
             Console.WriteLine("Welcome to Zork!");
 
             Room previousRoom = null;
@@ -127,25 +124,43 @@ namespace Zork
             }
         }
 
-        private static void InitializeRoomDescription()
+        private static readonly Dictionary<string, Room> RoomMap;
+
+        static Program()
         {
-            var roomMap = new Dictionary<string, Room>();
+            RoomMap = new Dictionary<string, Room>();
             foreach (Room room in rooms)
             {
-                roomMap.Add(room.Name, room);
+                RoomMap.Add(room.Name, room);
             }
+        }
 
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
-                    
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
-                    
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+        private enum CommandLineArguments
+        {
+            RoomsFilename = 0
+        }
+
+        private enum Fields
+        {
+            Name = 0,
+            Description
+        }
+
+        private static void InitializeRoomDescription(string roomsFilename)
+        {
+            const string fieldDelimiter = "##";
+            const int expectedFieldCount = 2;
+
+            var roomQuery = from line in File.ReadLines(roomsFilename)
+                            let fields = line.Split(fieldDelimiter)
+                            where fields.Length == expectedFieldCount
+                            select (Name: fields[(int)Fields.Name],
+                                    Description: fields[(int)Fields.Description]);
+
+            foreach (var (Name, Description) in roomQuery)
+            {
+                RoomMap[Name].Description = Description;
+            }
         }
 
         private static int currentRow = 1;
--- Zork/Room.cs
+++ Zork/Room.cs
@@ -1,10 +1,8 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace Zork
+﻿namespace Zork
 {
     internal class Room
     {
-        public string Name { get; }
+        public string Name { get; set; }
 
         public string Description { get; set; }
 
