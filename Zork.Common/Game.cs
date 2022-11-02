6652cc4dab0c40e8306a7e1e53da8e3a9ec4334c
MimiRaven <89601792+MimiRaven@users.noreply.github.com>
2022-11-02 15:22:06 -0400

    Assignment 9

---
+++ Zork.Common/Game.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace Zork.Common
+{
+    public class Game
+    {
+        public World World { get; }
+
+        public Player Player { get; }
+
+        public IOutputService Output { get; private set; }
+
+        public Game(World world, string startingLocation)
+        {
+            World = world;
+            Player = new Player(World, startingLocation);
+        }
+
+        public void Run(IOutputService output)
+        {
+            Output = output;
+
+            Room previousRoom = null;
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Output.WriteLine(Player.CurrentRoom);
+                if (previousRoom != Player.CurrentRoom)
+                {
+                    Output.WriteLine(Player.CurrentRoom.Description);
+                    previousRoom = Player.CurrentRoom;
+                }
+
+                Output.Write("> ");
+
+                string inputString = Console.ReadLine().Trim();
+                char  separator = ' ';
+                string[] commandTokens = inputString.Split(separator);
+                
+                string verb = null;
+                string subject = null;
+                if (commandTokens.Length == 0)
+                {
+                    continue;
+                }
+                else if (commandTokens.Length == 1)
+                {
+                    verb = commandTokens[0];
+
+                }
+                else
+                {
+                    verb = commandTokens[0];
+                    subject = commandTokens[1];
+                }
+
+                Commands command = ToCommand(verb);
+                string outputString;
+                switch (command)
+                {
+                    case Commands.Quit:
+                        isRunning = false;
+                        outputString = "Thank you for playing!";
+                        break;
+
+                    case Commands.Look:
+                        outputString = $"{ Player.CurrentRoom.Description}\n";
+                        foreach(Item item in Player.CurrentRoom.Inventory)
+                        {
+                            outputString += $"{item.Description}\n";
+                        }
+
+                        break;
+
+                    case Commands.North:
+                    case Commands.South:
+                    case Commands.East:
+                    case Commands.West:
+                        Directions direction = (Directions)command;
+                        if (Player.Move(direction))
+                        {
+                            outputString = $"You moved {direction}.";
+                        }
+                        else
+                        {
+                            outputString = "The way is shut!";
+                        }
+                        break;
+
+                    case Commands.Take:
+                        if (subject != null)
+                        {
+                            if (Player.Take(subject))
+                            {
+                                outputString = $"Taken {subject}";
+                            }
+                            else
+                            {
+                                outputString = "Cannot find object.";
+                            }
+                        }
+                        else
+                        {
+                            outputString = "What item are you trying take now?";
+                        }
+                        break;
+
+                    case Commands.Drop:
+                        if (subject != null)
+                        {
+                            if (Player.Drop(subject))
+                            {
+                                outputString = $"Dropped {subject}";
+                            }
+                            else
+                            {
+                                outputString = "Object cannot be found.";
+                            }
+                        }
+                        else
+                        {
+                            outputString = "There isn't anything that can be dropped.";
+                        }
+                        break;
+
+                    case Commands.Inventory:
+                        if(Player.Inventory.Count >= 1)
+                        {
+                            outputString = null;
+                            foreach (Item item in Player.Inventory)
+                            {
+                                outputString += item.Description;
+                            }
+                        }
+                        else
+                        {
+                            outputString = "You have nothing in your inventory.";
+                        }
+                        break;
+
+                    default:
+                        outputString = "Unknown command.";
+                        break;
+                }
+
+                Output.WriteLine(outputString);
+            }
+        }
+
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.Unknown;
+    }
+}
---
+++ Zork.Common/Player.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zork.Common
+{
+    public class Player
+    {
+        public Room CurrentRoom
+        {
+            get => _currentRoom;
+            set => _currentRoom = value;
+        }
+
+        public List<Item> Inventory { get; }
+
+
+        public Player(World world, string startingLocation)
+        {
+            _world = world;
+
+            if (_world.RoomsByName.TryGetValue(startingLocation, out _currentRoom) == false)
+            {
+                throw new Exception($"Invalid starting location: {startingLocation}");
+            }
+
+            Inventory = new List<Item>();
+        }
+
+        public bool Move(Directions direction)
+        {
+            bool didMove = _currentRoom.Neighbors.TryGetValue(direction, out Room neighbor);
+            if (didMove)
+            {
+                CurrentRoom = neighbor;
+            }
+
+            return didMove;
+        }
+
+        public bool Take(string itemName)
+        {
+            Item itemToTake = _world.ItemsByName.GetValueOrDefault(itemName);
+
+            if(CurrentRoom.Inventory.Contains(itemToTake))
+            {
+                Inventory.Add(itemToTake);
+                CurrentRoom.Inventory.Remove(itemToTake);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public bool Drop(string itemName)
+        {
+            Item itemToTake = _world.ItemsByName.GetValueOrDefault(itemName);
+
+                if (Inventory.Contains(itemToTake))
+            {
+                Inventory.Remove(itemToTake);
+                CurrentRoom.Inventory.Add(itemToTake);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private World _world;
+        private Room _currentRoom;
+    }
+}
